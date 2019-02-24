37a85947c0bb96e04c6c0ce91a4e97ac11029b83
utilibot <47805193+utilibot@users.noreply.github.com>
2019-02-24 18:48:09 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = "NTQ5MDM5MDE4NzQzMTY5MDM1.D1OEbg.sRjZ_kehag65vJwxRUzBwLs2j_Y" 
+const prefix = "dad";
+var rand =["dadjoke 1", "dadjoke 2"];
+var pun =["dadpun 1", "dadpun 2"];
+
+
+client.on('ready', () => {
+    client.user.setActivity("dadhelp"); 
+    });
+    client.on("message", async (message) => {
+        if (message.author.bot) return;
+        if (message.content.startsWith(`${prefix}help`)) {
+            var embed = new Discord.RichEmbed()
+                .setColor(0x252629)
+                .setAuthor(client.user.username, client.user.avatarURL)
+                .setTitle("Help Menu")
+                .setDescription("The Help Menu for DadBot")
+                .addField("**Help**", "Shows the help menu.")
+                .addField("**Joke**", "Tells a dad joke")
+                .addField("**Pun**", "Tells a pun")
+                .addField("**Ping**", "Gets the ping of the bot.")
+                .setTimestamp()
+                .setFooter("Created by brickman#4669", client.user.avatarURL)
+                message.channel.send(embed)
+            
+        } else if (message.content.startsWith(`${prefix}joke`)) {
+            var repl = rand[Math.floor(Math.random()*rand.length)];
+        message.channel.send(repl);
+    } else if (message.content.startsWith(`${prefix}pun`)) {
+        var repl2 = pun[Math.floor(Math.random()*pun.length)];
+        message.channel.send(repl2);
+    } else if (message.content.startsWith(`${prefix}ping`)) {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+        }});
+        client.login(token)

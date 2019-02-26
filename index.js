d4d3943012a3b94a807e585508ab3ec31e1c5d44
utilibot <47805193+utilibot@users.noreply.github.com>
2019-02-26 18:10:52 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prefix = "dad ";
 var rand =["With great power comes a huge electricity bill.", "I killed a piece of paper. RIP", "Why can’t you stay up until the cows come home? Because it's pasture bedtime.", "A horse walks into a bar. The bartender says `Hey!` Then the horse replies `Sounds good!`", "A dad was washing his car with his son. The son asked why he couldn't just use a sponge.", "I dont know why drunks fail the line test. It's really straight forward.", "What do you call an alligator wearing a vest? An Investigator!", "I never learned when to properly use contractions but that is ok. It's what it's", "Think of a number between 5 and 15. Multiply by 2, add 3, and subtract 7 from the answer. Now close your eyes. Dark, isn't it?" ,"My car horn wasn’t working, so I took it to a Boy Scout. He said `Beep repaired`", "Did you hear about the drummer who had twin daughters? He named them Anna 1, Anna 2"];
 var pun =["The first computer dates back to Adam and Eve. It was an Apple with limited memory, just one byte. And then everything crashed.", "About a month before he died, my uncle had his back covered in lard. After that, he went down hill fast.", "`Doctor, there`s a patient on line 1 who says he`s invisible. Doctor: Well I can`t see him right now."]
 
-"];
+];
 
 
 client.on('ready', () => {
